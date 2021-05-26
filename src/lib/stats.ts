edee356eb137f7921883a256c09553a4af49cb3b
Jdevdisc <69683068+JDevelo@users.noreply.github.com>
2021-05-26 00:13:48 -0400

    update

---
+++ src/lib/stats.ts
@@ -0,0 +1,43 @@
+export default class dbotsStats {
+    /**
+ * fetch
+ */
+    public fetch;
+    /**
+     * Key
+     */
+    public key: null;
+    /**
+     * botid
+    */
+    public botid: null;
+    constructor(ops = {
+        key: null as any,
+        botid: null as any
+    }) {
+        /**
+         * Auth key
+         */
+        this.key = ops.key
+        /**
+         * Bot ID
+         */
+        this.botid = ops.botid
+        /**
+         * node fetch
+         */
+        this.fetch = require('node-fetch')
+    }
+    async postStats(servercount?: string) {
+        this.fetch(`https://dbots.co/api/v1/bots/${this.botid}/stats`, {
+            method: 'POST',
+            headers: {
+                "Authorization": `${this.key}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                guildCount: servercount
+            })
+        })
+    }
+}
